483826deb0a7ac107ea87867941026486fb9fe84
trickerer <onlysuffering@gmail.com>
2013-09-27 00:20:27 +0700

    Bots' equipment change implementation. New feature allows players to give weapons to their npcbots. Item disappears from player's inventory and can be retrieved from bot at any moment. Warning: this will destroy any enchantments and creator flags on item! Also, bots are no longer removed from DB, instead they are marked as inactive and become active (autocreated at login) again at next summon. This will ensure bots save their custom equipment FOREVER until table deletion. Tested at 3.3.5. SQLs updated.

--- SQL/4.3.4/character_NPC_bots.sql
+++ SQL/4.3.4/character_NPC_bots.sql
@@ -5,5 +5,9 @@ CREATE TABLE `character_npcbot` (
   `race` tinyint(3) default NULL,
   `class` tinyint(3) default NULL,
   `istank` tinyint(3) default NULL,
+  `equipMhEx` int(10) default NULL,
+  `equipOhEx` int(10) default NULL,
+  `equipRhEx` int(10) default NULL,
+  `active` tinyint(3) default NULL,
   PRIMARY KEY  (`owner`,`entry`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8;
