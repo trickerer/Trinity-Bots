c913db4c596e62a136cc77581546d536522b5302
trickerer <onlysuffering@gmail.com>
2013-10-17 11:52:55 +0700

    [335] Forgot to update SQLs /facepalm

--- SQL/3.3.5/auth_rbac.sql
+++ SQL/3.3.5/auth_rbac.sql
@@ -13,15 +13,15 @@ INSERT INTO `rbac_permissions` (`id`,`name`) VALUES
 (@NPCB_PERM+8,'npcbot revive'),
 (@NPCB_PERM+9,'maintank (mt)');
 
-DELETE FROM `rbac_role_permissions` WHERE `permissionId` BETWEEN @NPCB_PERM AND @NPCB_PERM+9;
-INSERT INTO `rbac_role_permissions` (`roleId`,`permissionId`) VALUES
-('1',@NPCB_PERM+0),
-('1',@NPCB_PERM+1),
-('1',@NPCB_PERM+2),
-('1',@NPCB_PERM+3),
-('1',@NPCB_PERM+4),
-('1',@NPCB_PERM+5),
-('1',@NPCB_PERM+6),
-('1',@NPCB_PERM+7),
-('2',@NPCB_PERM+8),
-('1',@NPCB_PERM+9);
+DELETE FROM `rbac_linked_permissions` WHERE `linkedId` BETWEEN @NPCB_PERM AND @NPCB_PERM+9;
+INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
+('199',@NPCB_PERM+0), -- player command .npcbot
+('199',@NPCB_PERM+1), -- player command .npcbot add
+('199',@NPCB_PERM+2), -- player command .npcbot remove
+('199',@NPCB_PERM+3), -- player command .npcbot reset
+('199',@NPCB_PERM+4), -- player command .npcbot command
+('199',@NPCB_PERM+5), -- player command .npcbot distance
+('199',@NPCB_PERM+6), -- player command .npcbot info
+('199',@NPCB_PERM+7), -- player command .npcbot helper
+('198',@NPCB_PERM+8), -- moderator command .npcbot revive
+('199',@NPCB_PERM+9); -- player command .npcbot .maintank (mt)
