82d2382ad8bda8c92cef57ded0d8228c8808d05d
trickerer <onlysuffering@gmail.com>
2014-06-11 22:01:19 +0700

    NPCBots v4.0.16a
    Make bots static in world (Closes #77, Closes #72, Closes #89, Closes #90, Closes #68, Closes #96, Closes #101, Closes #104). Drop 4.3.4 branch support (Closes #74, Closes #75, Closes #85, Closes #87) Add basic honor for bots (Closes #30). Bots' stats are no longer depend on players (Closes #83, Ref #97, Ref #98). Convert equips from ItemTemplate* from Item* and make bots use real items and benefit from all items properties except certain class ability improvements (increase damage, decrease cast time, etc.) (Closes #51). Add Auto-equip option, allows to quick-equip items with simple item comparison algorithm (item level) (Closes #63). Add 'Unequip all' option, allows to unequip all items (Closes #95). Make npcbots' stats fully player-like (Ref #97, Closes #98). Disable custom cooldowns (Closes #97, Closes #100). Modify bots' formation to never spread too much (Closes #99). Allow bots to attack neutral units (Closes #107). Allow npcbots to queue into Dungeon Finder with master (Closes #20). No manual yet

--- SQL/3.3.5/updates/characters/2013_12_15_00_character_npcbots.sql
+++
@@ -1,7 +0,0 @@
-SET @ROLE_DPS:=2;
-SET @ROLE_HEAL:=4;
-SET @ROLE_RANGED:=8;
-
-ALTER TABLE `character_npcbot` CHANGE `istank` `roles` tinyint(3) NULL;
-UPDATE `character_npcbot` SET `roles`=`roles` | @ROLE_DPS;
-UPDATE `character_npcbot` SET `roles`=`roles` | @ROLE_HEAL | @ROLE_RANGED WHERE `class` IN (2,5,7,11);
