4688efa40dc4ba1804c6d2b89f951f997bf90ab4
trickerer <onlysuffering@gmail.com>
2019-06-14 17:29:09 +0700

    NPCBots v4.0.591a
    Changes&Fixes:
    
    Fixed a rare crash at startup
    Default faction set to 35
    High-leveled equipment replaced with low-leveled
    Proper races assigned
    Damage at starting levels normalized
    Potions/rations converted to player ones available at given level
    Health and powers regeneration are fixed
    Bots are now less wasteful with their interruption/silence/CC spells
    Spell and global cooldowns now work as intended
    Bot movement is now more stable and positioning is more precise while player is moving; angle at which bots position themselves is now based on their roles
    Healing/Buffing is now more efficient
    Bots can now resurrect other bots
    Bots can now recognize spell reflection effects on their targets and burn them down
    Stat system is reworked
    Bots now recognize units targeted by your negative spells as valid attack targets (as if you targeted said unit with autoattack)
    Bots now only attack bosses if someone is tanking well enough (has enough threat)
    Bots now enter and leave combat as players do (5 sec PvP timer)
    Bots' speed is now not that much higher than yours
    Bots now benefit from talents with spell range, radius and max targets bonuses
    Gossip menu now allows you to add all bots to group at once
    Gossip menu now allows you to check what aura/aspect/etc. your seventh paladin/hunter/etc. is currently using
    Bots now more smart with their spell casting and will interrupt current spell if it has become pointless
    Bots now try to heal tanks preemptively if they are tanking something serious
    Bot cost is now non-linear until level 40 (10s at level 1)
    Bots now queue in LFG by roles they could have, not roles assigned
    Tons of other small and not-so-small fixes and improvements
    
    New stuff:
    
    Gathering professions: bots can detect and gather stuff for you (you have to find it first); skill level is maximum for max profession rank available at current level
    Kill counters (not saved between server restarts)
    Defense skill (yes, defcap on bot is now possible)
    You can now give consumables to your bots (through gossip menu). Item is not taken (yet) since bot buffs are not saved (...yet?)
    You can now ask Rogue's help with locked stuff (skill level depends on bot level); fail is rolled twice
    You can now request a healthstone from a Warlock bot; healthstone level is of course depends on bot level
    Your Rogues and Shamans now poison/enchant their weapons. Your can choose for them what to use. If you give an enchanted weapon to bot it will never expire
    You can now manually remove unwanted buffs from your bots through troubleshooting gossip menu ('pull yourself together')
    Most item links now contain item icon (both menu and chat)
    Racial abilities
    New '.npcbot' commands; 'info': quick info about owned bots, 'command' ('follow', 'stay'), 'hide'/'unhide'('show'): removes bots from world temporarily; troubleshooting commands: 'recall': makes bots to move to your exact position (usable while dead), 'kill'/'suicide': kills selected (your) bot.
    Completely reworked classes: Druid, Hunter, Rogue, Shaman
    Now so completely but at least half reworked classes: Mage, Paladin

--- SQL/character_bots.sql
+++ SQL/character_bots.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS `characters_npcbot`;
 CREATE TABLE `characters_npcbot` (
   `entry` int(10) unsigned NOT NULL COMMENT 'creature_template.entry',
   `owner` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'characters.guid (lowguid)',
-  `roles` tinyint(3) unsigned NOT NULL COMMENT 'bitmask: tank(1),dps(2),heal(4),ranged(8)',
-  `faction` int(10) unsigned NOT NULL DEFAULT '14',
+  `roles` smallint(5) unsigned NOT NULL COMMENT 'bitmask: tank(1),dps(2),heal(4),ranged(8)',
+  `faction` int(10) unsigned NOT NULL DEFAULT '35',
   `equipMhEx` int(10) unsigned NOT NULL DEFAULT '0',
   `equipOhEx` int(10) unsigned NOT NULL DEFAULT '0',
   `equipRhEx` int(10) unsigned NOT NULL DEFAULT '0',
