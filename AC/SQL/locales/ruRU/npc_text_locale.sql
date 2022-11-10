9eb9aed1c003617bdfcce8243179a1a2a8d4e81c
Barselon <laa151283@gmail.com>
2022-11-10 19:57:34 +0500

    -

--- AC/SQL/locales/ruRU/npc_text_locale.sql
+++ AC/SQL/locales/ruRU/npc_text_locale.sql
@@ -1,4 +1,4 @@
-DELETE FROM `npc_text_locale` WHERE `Locale`='ruRU' AND `ID` BETWEEN '70000' AND '71000';
+DELETE FROM `npc_text_locale` WHERE `Locale`='ruRU' AND `ID` BETWEEN '70300' AND '71000';
 INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`, `Text1_0`, `Text1_1`, `Text2_0`, `Text2_1`, `Text3_0`, `Text3_1`, `Text4_0`, `Text4_1`, `Text5_0`, `Text5_1`, `Text6_0`, `Text6_1`, `Text7_0`, `Text7_1`)
 VALUES
 (70300, 'ruRU', 'Умри!', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
